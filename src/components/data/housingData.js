8bffd85e00201e9828f680b4f1277d7ce1e85608
Illyriat <109182598+Illyriat@users.noreply.github.com>
2024-04-05 06:37:15 +0100

    Housing Data structure complete

--- src/components/data/housingData.js
+++ src/components/data/housingData.js
@@ -1,12 +1,130 @@
-export const housingData = [
+// Home Types split into their own groups for easier additions in the future.
+// As the Housing collections grow, it may become reasonable to split these off into their own grouped
+// pages.
+
+export const innRooms = [
+
+    // Inn Rooms x 10
     {
         id: 1,
         homeName: "",
         zoneLocation: "",
         homeType: "",
-        nonPlusSize: "",
-        plusSize: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
         goldCost: "",
-        crownCost: ""
-    }
+        crownCost: "",
+        playerCapacity: "",
+    },
+]
+
+
+export const apparentments = [
+    // Apartments x 3
+    {
+        id: 1,
+        homeName: "",
+        zoneLocation: "",
+        homeType: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
+        goldCost: "",
+        crownCost: "",
+        playerCapacity: "",
+    },
+]
+
+
+export const smallHomes = [
+    // Small Houses x 10
+    {
+        id: 1,
+        homeName: "",
+        zoneLocation: "",
+        homeType: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
+        goldCost: "",
+        crownCost: "",
+        playerCapacity: "",
+    },
+]
+
+
+
+export const mediumHomes = [
+    // Medium Houses x 12
+    {
+        id: 1,
+        homeName: "",
+        zoneLocation: "",
+        homeType: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
+        goldCost: "",
+        crownCost: "",
+        playerCapacity: "",
+    },
+]
+
+
+export const largeHomes = [
+    // Large Houses x 16
+    {
+        id: 1,
+        homeName: "",
+        zoneLocation: "",
+        homeType: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
+        goldCost: "",
+        crownCost: "",
+        playerCapacity: "",
+    },
+]
+
+
+export const manorHomes = [
+    // Manors x 7
+    {
+        id: 1,
+        homeName: "",
+        zoneLocation: "",
+        homeType: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
+        goldCost: "",
+        crownCost: "",
+        playerCapacity: "",
+    },
+]
+
+
+export const crownStoreHomes = [
+    // Crown Store Homes x 41
+    {
+        id: 1,
+        homeName: "",
+        zoneLocation: "",
+        homeType: "",
+        furnishingSize: "",
+        specialFurnishing: "",
+        collectableFurnishing: "",
+        specialCollectables: "",
+        goldCost: "",
+        crownCost: "",
+        playerCapacity: "",
+    },
 ]
